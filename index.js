636cc2fb72e8bd94a382dba1ec664650909e642d
JDGassiraro <josephgassiraro2021@gmail.com>
2023-07-19 21:13:24 -0500

    working SPA of Savvy Coders Example Website

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+import Navigo from "navigo";
+import {capitalize} from "lodash";
+import { Header, Nav, Main, Footer } from "./components";
+import * as store from "./store";
+
+const router = new Navigo("/");
+
+router
+.on({
+  "/": () => render(store.Home),
+  ":view": (params) => {
+    let view = capitalize(params.data.view);
+    if (store.hasOwnProperty(view)) {
+      render(store[view]);
+    } else {
+      render(store.Viewnotfound);
+      console.log(`View ${view} not defined`);
+    }
+  },
+})
+.resolve();
+
+function render(state = store.Home) {
+  document.querySelector("#root").innerHTML = `
+    ${Header(state)}
+    ${Nav(store.Links)}
+    ${Main(state)}
+    ${Footer()}
+  `;
+
+  router.updatePageLinks();
+}
+
 // add menu toggle to bars icon in nav bar
 document.querySelector(".fa-bars").addEventListener("click", () => {
   document.querySelector("nav > ul").classList.toggle("hidden--mobile");
